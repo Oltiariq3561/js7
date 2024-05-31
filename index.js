27d2cba8d3f60dfc392ed9769c28d48c25919f5f
Oltiariq3561 <oltiariq356@gmail.com>
2024-05-31 00:41:55 -0700

    jebihbe

---
+++ index.js
@@ -0,0 +1,251 @@
+// let a = 1;
+
+// let b = 30;
+
+// let count=a;
+
+// let result=1;
+
+// while (count <= b) {
+//     if(count % 3 ==0 && count % 5 ==0){
+//     result=result+count;
+// }
+//     count++;
+// }
+
+// console.log(result);
+
+// switch (a) {
+//     case 1:
+//         console.log('yanvar');
+//         break;
+//     case 2:
+//         console.log('fevral');
+//         break;
+//     case 3:
+//         console.log('mart');
+//         break;
+//     case 4:
+//         console.log('aprel');
+//         break;
+//     case 5:
+//         console.log('may');
+//         break;
+//     case 6:
+//         console.log('iyun');
+//         break;
+//     case 7:
+//         console.log('iyul');
+//         break;
+//     case 8:
+//         console.log('avgust');
+//         break;
+//     case 9:
+//         console.log('sentyabr');
+//         break;
+//     case 10:
+//         console.log('oktyabr');
+//         break;
+//     case 11:
+//         console.log('noyabr');
+//         break;
+//     case 12:
+//         console.log('dekabr');
+//         break;
+//     default:
+//         console.log('12 ta oy boladi xalos!!!');
+//         break;
+// }
+
+
+// 2-masala
+// function name() {
+//     let a = +prompt('ball ni kiriting');
+
+// switch (a) {
+//     case 90:
+//     case 91:
+//     case 92:
+//     case 93:
+//     case 94:
+//     case 95:
+//     case 96:
+//     case 97:
+//     case 98:
+//     case 99:
+//     case 100:
+//         console.log('A');
+//         break;
+//     case 80:
+//     case 81:
+//     case 82:
+//     case 83:
+//     case 84:
+//     case 85:
+//     case 86:
+//     case 87:
+//     case 88:
+//     case 89:
+//     case 90:
+//         console.log('B');
+//         break;
+//     case 70:
+//     case 71:
+//     case 72:
+//     case 73:
+//     case 74:
+//     case 75:
+//     case 76:
+//     case 77:
+//     case 78:
+//     case 79:
+//     case 80:
+//         console.log('C');
+//         break;
+
+//     case 60:
+//     case 61:
+//     case 62:
+//     case 63:
+//     case 64:
+//     case 65:
+//     case 66:
+//     case 67:
+//     case 68:
+//     case 69:
+//     case 70:
+//         console.log('D');
+//         break;
+//     case 0:
+//     case 1:
+//     case 2:
+//     case 3:
+//     case 4:
+//     case 5:
+//     case 6:
+//     case 7:
+//     case 8:
+//     case 9:
+//     case 10:
+//     case 11:
+//     case 12:
+//     case 13:
+//     case 14:
+//     case 15:
+//     case 16:
+//     case 17:
+//     case 18:
+//     case 19:
+//     case 20:
+//     case 21:
+//     case 22:
+//     case 23:
+//     case 24:
+//     case 25:
+//     case 26:
+//     case 27:
+//     case 28:
+//     case 29:
+//     case 30:
+//     case 31:
+//     case 32:
+//     case 33:
+//     case 34:
+//     case 35:
+//     case 36:
+//     case 37:
+//     case 38:
+//     case 39:
+//     case 40:
+//     case 41:
+//     case 42:
+//     case 43:
+//     case 44:
+//     case 45:
+//     case 46:
+//     case 47:
+//     case 48:
+//     case 49:
+//     case 50:
+//     case 51:
+//     case 52:
+//     case 53:
+//     case 54:
+//     case 55:
+//     case 56:
+//     case 57:
+//     case 58:
+//     case 59:
+//     case 60:
+//         console.log('F');
+//         break
+//     default:
+//         break;
+// }
+
+// }
+// name()
+
+// 3-masala
+// let a = + prompt('son')
+
+// switch (a) {
+//     case 5:
+//     case 6:
+//     case 7:
+//     case 8:
+//     case 9:
+//     case 10:
+//     case 10:
+//     case 11:
+//     case 12:
+//         console.log('xayrli tong');
+//         break;
+//     case 13:
+//     case 14:
+//     case 15:
+//     case 16:
+//     case 17:
+//         console.log('xayrli kun');
+//         break;
+
+//     case 18:
+//     case 19:
+//     case 20:
+//     case 21:
+//         console.log('xayrli kech');
+//         break;
+
+//     case 22:
+//     case 23:
+//     case 24:
+//     case 0:
+//     case 1:
+//     case 2:
+//     case 3:
+//     case 4:
+//     case 5:
+//         console.log('xayrli tun');
+//         break;
+
+
+//     default:
+//         console.log('24 soat boladi xolos');
+//         break;
+// }
+
+// 5-masala
+
+function name() {
+    
+let a = 0;
+
+while (a<=20) {
+    if (a%2==0) {
+        console.log(a);
+      
+    }
+      a++;
+}
+
+}
