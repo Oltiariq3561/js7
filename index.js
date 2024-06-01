7130ba742e1a7ae0233eb1c7e3a776e13893af79
Oltiariq3561 <oltiariq356@gmail.com>
2024-05-31 22:15:37 -0700

    salom

--- index.js
+++ index.js
@@ -2,14 +2,14 @@
 
 // let b = 30;
 
-// let count=a;
+// let count = a;
 
-// let result=1;
+// let result = 1;
 
 // while (count <= b) {
-//     if(count % 3 ==0 && count % 5 ==0){
-//     result=result+count;
-// }
+//     if (count % 3 == 0 && count % 5 == 0) {
+//         result = result + count;
+//     }
 //     count++;
 // }
 
@@ -57,73 +57,78 @@
 //         break;
 // }
 
-// 2-masala
-// let a =+prompt('ballni yozing')
-// switch (true) {
-//     case a>=90 && a<=100:
-//         console.log('A');
-//         break;
-//     case a >= 80 && a<90:
-//         console.log('B');
-//         break;
-//     case a >+ 70 && a<80:
-//         console.log('C');
-//         break;
-//     case a >= 60 && a<70:
-//         console.log('D');
-//         break;
-//     case a >= 0  && a<60:
-//         console.log('F');
-//         break
-//     default:
-//         break;
-// }
+// 2 - masala
+// function ball(params) {
 
-// 3-masala
+
+//     let a = +prompt('ballni yozing')
+//     switch (true) {
+//         case a >= 90 && a <= 100:
+//             console.log('A');
+//             break;
+//         case a >= 80 && a < 90:
+//             console.log('B');
+//             break;
+//         case a > + 70 && a < 80:
+//             console.log('C');
+//             break;
+//         case a >= 60 && a < 70:
+//             console.log('D');
+//             break;
+//         case a >= 0 && a < 60:
+//             console.log('F');
+//             break
+//         default:
+//             break;
+//             return a
+//     }
+// }
+// ball()
+// 3 - masala
 
 // function vaqt() {
-    
+
 //     let a = + prompt('vaqt kiriting');
 
-// switch (true) {
-//     case a>5 && a<=12:
-//         console.log('xayrli tong!!!');
-//         break;
-//     case a>12 && a<=17:
-//         console.log('xayrli kun!!!');
-//         break;
-//     case a>17 && a<=21:
-//     console.log('xayrli kech!!!');
-//     break;
-//     case a>21 && a<=24:
-//         console.log('xayrli tun!!!');
-//         break;
-//     default:
-//         console.log('bunday vaqt mavjud emas 0 dan 24 gacha son yozing');
-//         break;
+//     switch (true) {
+//         case a > 5 && a <= 12:
+//             console.log('xayrli tong!!!');
+//             break;
+//         case a > 12 && a <= 17:
+//             console.log('xayrli kun!!!');
+//             break;
+//         case a > 17 && a <= 21:
+//             console.log('xayrli kech!!!');
+//             break;
+//         case a > 21 && a <= 24:
+//             console.log('xayrli tun!!!');
+//             break;
+//         default:
+//             console.log('bunday vaqt mavjud emas 0 dan 24 gacha son yozing');
+//             break;
 
-        
-// }
+
+//     }
 // }
 // vaqt()
-// 4-masala
+// 4 - masala
 
 // let raqam = prompt('raqam yozing');
 
 // function togriRaqam() {
-//     while(true){
-//         if(raqam.toLowerCase()==5){
-//          alert(raqam);
-//          break;
-//         }else{
-//         raqam = prompt('raqam yozing')
+//     while (true) {
+//         if (raqam.toLowerCase() == 5) {
+//             alert(raqam);
+//             break;
+//         } else {
+//             raqam = prompt('raqam yozing')
 //         }
 //     }
 // }
 
 // togriRaqam();
 
-// 5-masala
+// 5 - masala
 
 // function name() {
 //     let a = 20;
@@ -131,23 +136,23 @@
 //     let sum = 0;
 //     while (i <= a) {
 //         console.log(i);
-//         i+=2;
+//         i += 2;
 //     }
 
 // }
 // name()
 
-// 6-masala
+// 6 - masala
 
 // let soz = prompt('soz yozing');
 
 // function togriSoz() {
-//     while(true){
-//         if(soz.toLowerCase()=="stop"){
-//          alert(soz);
-//          break;
-//         }else{
-//         soz = prompt('soz yozing')
+//     while (true) {
+//         if (soz.toLowerCase() == "stop") {
+//             alert(soz);
+//             break;
+//         } else {
+//             soz = prompt('soz yozing')
 //         }
 //     }
 // }
